6412270c7de82e84b6c4b0b5df6f8fd217510274
Nolann Laine <nolann.laine@alstomgroup.com>
2026-02-27 14:57:37 +0100

    change App to flex-1 to increase footer integration

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
   const [page, setPage] = useState<Page>("Accueil");
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex-1">
       {/* Topbar */}
       <TopBar page={page} setPage={setPage} />
 
